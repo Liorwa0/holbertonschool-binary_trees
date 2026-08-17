ff8fc9635b74e47b77470dcd617e3e3c67fa0ff1
zaslse <zaslse@hotmail.com>
2026-08-17 14:13:48 +0000

    Fix dependencies: Add balance and rotation functions

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,55 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- * Return: Height of the tree, or 0 if tree is NULL
+ * binary_tree_height_b - Measures height of a binary tree for a balanced tree
+ * @tree: tree to go through
+ * Return: the height
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height_b(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t l = 0, r = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
-	else
-		left_height = 0;
-
-	if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
-	else
-		right_height = 0;
-
-	if (left_height > right_height)
-		return (left_height);
-	return (right_height);
+	l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+	r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+	return ((l > r) ? l : r);
 }
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
- * Return: Balance factor, or 0 if tree is NULL
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_h = 0, right_h = 0;
+	int left = 0, right = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left)
-		left_h = 1 + binary_tree_height(tree->left);
-	else
-		left_h = 0;
-
-	if (tree->right)
-		right_h = 1 + binary_tree_height(tree->right);
-	else
-		right_h = 0;
-
-	return (left_h - right_h);
+	if (tree)
+	{
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		return (left - right);
+	}
+	return (0);
 }
-
